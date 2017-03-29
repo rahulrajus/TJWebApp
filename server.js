9c758cb5997e8175a2338a56c0cb4b5653a93a14
Rahul Rajan <rahul.rajanus@gmail.com>
2017-03-29 11:01:05 -0400

    bort bive 000

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.get('/', function (req, res) {
 //   res.sendFile(__dirname + "/summer.html");
 // });
 
-app.listen(5000, function () {
+app.listen(process.env.PORT || 5000, function () {
   console.log('Example app listening on port 3000!')
 })
